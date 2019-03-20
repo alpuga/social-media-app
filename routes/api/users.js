3f39658a880c631ccbacaf25bcae9405914fc904
Alex <alpuga8@gmail.com>
2019-03-20 11:45:34 -0400

    Email & Password Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,4 +46,28 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route  POST api/users/login
+// @desc   login User
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by Email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found." });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect." });
+      }
+    });
+  });
+});
+
 module.exports = router;
