ab514bd0b66d21e8282ccc7c24f7636ac0f37682
Alex <alpuga8@gmail.com>
2019-03-21 10:25:27 -0400

    Passport JWT Authentication Strategy

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 // db configuration
 const db = require("./config/keys").mongoURI;
 
