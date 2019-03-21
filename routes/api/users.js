a63fa2c7ae711c7d3a9ebe2e6dd1cd1f6d64ec06
Alex <alpuga8@gmail.com>
2019-03-21 08:39:02 -0400

    Validation Handlers

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 const router = express.Router();
 
 const User = require("../../models/User");
@@ -18,9 +22,16 @@ router.get("/test", (req, res) => res.json({ msg: "users works" }));
 // @desc   Register User
 // @access Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // size
@@ -58,8 +69,15 @@ router.post("/login", (req, res) => {
 
   // Find user by Email
   User.findOne({ email }).then(user => {
+    const { errors, isValid } = validateLoginInput(req.body);
+    // Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
     if (!user) {
-      return res.status(404).json({ email: "User not found." });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -80,7 +98,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect." });
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
