ae3334c1d747118e1d45e7c615f7cd77eb51a09c
Alex <alpuga8@gmail.com>
2019-03-24 15:40:47 -0400

    Testing Registration With Form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class Register extends Component {
   constructor(props) {
@@ -31,7 +32,10 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    console.log(newUser);
+    axios
+      .post("/api/users/register", newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
   }
 
   render() {
