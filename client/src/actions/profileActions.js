7c38f8ffa725d5c4774ac11f34842a17ddb5fce5
Alex <alpuga8@gmail.com>
2019-03-28 18:18:51 -0400

    Add Education Form & Functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -62,6 +62,18 @@ export const addExperience = (expData, history) => dispatch => {
     );
 };
 
+// Add education
+export const addEducation = (eduData, history) => dispatch => {
+  axios
+    .post("/api/profile/education", eduData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 // Delete account & Profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This CANNOT be undone.")) {
